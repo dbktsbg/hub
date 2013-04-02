e36fed4bfa2924347f2a5a9395661921178e9fa6
david <dbkdev50@gmail.com>
2013-04-02 08:46:13 -0400

    fixed layout & button

--- www/js/hub.js
+++ www/js/hub.js
@@ -8,7 +8,7 @@ function InitializeHub()
     var DeviceName = device.name
 
     jQuery("#DeviceNameRegion").text("Device Name: " + DeviceName);
-    jQuery("#DeviceNameRegion").text("Device ID: " + DeviceID);
+    jQuery("#DeviceIDRegion").text("Device ID: " + DeviceID);
 
     jQuery("#LaunchCloudVoteAnchorButton").attr("href", "http://www.meridiaars.com");
     jQuery("#LaunchCloudVoteAnchorButton").text("Join Meeting...");
