9650e64e8e470df02a4b75cfc2fa4b5e43cdb0f2
david <dbkdev50@gmail.com>
2013-04-02 12:20:30 -0400

    jquery redirect

--- www/js/hub.js
+++ www/js/hub.js
@@ -13,19 +13,22 @@ function SetupToLaunchCloudVoteParticipant()
     jQuery("#DeviceNameRegion").text("Device Name: " + DeviceName);
     jQuery("#DeviceIDRegion").text("Device ID: " + DeviceID);
 
-    if (DeviceID=="ef8048728a9fcdbb")
-    {
-        jQuery("#LaunchCloudVoteAnchorButton").attr("href", "http://www.meridiaars.com/cv1.html");
-        window.location.replace("http://www.meridiaars.com/cv1.html");
-    }
+//    if (DeviceID=="ef8048728a9fcdbb")
+//    {
+//        jQuery("#LaunchCloudVoteAnchorButton").attr("href", "http://www.meridiaars.com/cv1.html");
+//        window.location.replace("http://www.meridiaars.com/cv1.html");
+//    }
 
-    if (DeviceID == "1875fec3215046cd") 
-    {
-        jQuery("#LaunchCloudVoteAnchorButton").attr("href", "http://www.meridiaars.com/cv2.html");
-        window.location.replace("http://www.meridiaars.com/cv2.html");
-    }
+//    if (DeviceID == "1875fec3215046cd") 
+//    {
+//        jQuery("#LaunchCloudVoteAnchorButton").attr("href", "http://www.meridiaars.com/cv2.html");
+//        window.location.replace("http://www.meridiaars.com/cv2.html");
+//    }
 
-    jQuery("#LaunchCloudVoteAnchorButton").attr("href", "http://www.cloud-vote");
-    window.location.replace("http://www.cloud-vote");
+//    jQuery("#LaunchCloudVoteAnchorButton").attr("href", "http://www.cloud-vote");
+//    window.location.replace("http://www.cloud-vote");
+
+    var url = "http://www.meridiaars.com/cv2.html";
+    $(location).attr('href', url);
 
 }
