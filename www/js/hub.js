1e2686834fb734222d93feead1a4544b85639ae3
david <dbkdev50@gmail.com>
2013-04-02 12:15:18 -0400

    direct urls

--- www/js/hub.js
+++ www/js/hub.js
@@ -9,22 +9,23 @@ function SetupToLaunchCloudVoteParticipant()
 
     var DeviceID = device.uuid
     var DeviceName = device.name
-    var CloudVoteURL  = "http://www.cloud-vote"
 
     jQuery("#DeviceNameRegion").text("Device Name: " + DeviceName);
     jQuery("#DeviceIDRegion").text("Device ID: " + DeviceID);
 
     if (DeviceID=="ef8048728a9fcdbb")
     {
-        CloudVoteURL = "http://www.meridiaars.com/cv1.html");
+        jQuery("#LaunchCloudVoteAnchorButton").attr("href", "http://www.meridiaars.com/cv1.html");
+        window.location.replace("http://www.meridiaars.com/cv1.html");
     }
 
     if (DeviceID == "1875fec3215046cd") 
     {
-       CloudVoteURL = "http://www.meridiaars.com/cv2.html");
+        jQuery("#LaunchCloudVoteAnchorButton").attr("href", "http://www.meridiaars.com/cv2.html");
+        window.location.replace("http://www.meridiaars.com/cv2.html");
     }
 
-    jQuery("#LaunchCloudVoteAnchorButton").attr("href", CloudVoteURL);
+    jQuery("#LaunchCloudVoteAnchorButton").attr("href", "http://www.cloud-vote");
+    window.location.replace("http://www.cloud-vote");
 
-    window.location.replace(CloudVoteURL);
 }
