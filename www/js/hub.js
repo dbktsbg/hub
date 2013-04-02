2057db27153c2506101bc41e001d713b8bfd2d42
david <dbkdev50@gmail.com>
2013-04-02 08:18:28 -0400

    new button approach

--- www/js/hub.js
+++ www/js/hub.js
@@ -1,32 +1,16 @@
 
 function InitializeHub() 
 {
+
     alert("InitializeHub");
 
     var DeviceID = device.uuid
     var DeviceName = device.name
 
-    jQuery("#AppRegion").html
-                                        (
-                                        "<a " +
-                                            "data-role=\"button\" " +
-                                            "data-theme=\"b\" " +
-                                            "href=\"meridiaars.com\" " + 
-                                        ">" +
-                                            "Join Meeting using Device: " +
-                                            DeviceName + " (" + DeviceID + ")" +
-                                        "</a>"
-                                        );
+    jQuery("#DeviceNameRegion").text("Device Name: " + DeviceName);
+    jQuery("#DeviceNameRegion").text("Device ID: " + DeviceID);
 
+    jQuery("#LaunchCloudVoteAnchorButton").href = "meridiaars.com";
+    jQuery("#LaunchCloudVoteAnchorButton").text("Join Meeting...");
 
-//                            <a 
-//                            href="http:/www.cloud-vote.com/remote" 
-//                            data-role="button"
-//                            data-theme="b">Join Meeting Now...</a>
-
-}
-
-function LaunchCloudVoteParticipant(DeviceID, DeviceName)
-{
-    
 }
