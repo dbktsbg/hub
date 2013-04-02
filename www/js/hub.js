25a32b669c25e63cebe8c62774542d23caaa818b
david <dbkdev50@gmail.com>
2013-04-02 11:44:36 -0400

    windows.location

--- www/js/hub.js
+++ www/js/hub.js
@@ -9,22 +9,25 @@ function SetupToLaunchCloudVoteParticipant()
 
     var DeviceID = device.uuid
     var DeviceName = device.name
+    var CloudVoteURL  = "http://www.cloud-vote"
 
     jQuery("#DeviceNameRegion").text("Device Name: " + DeviceName);
     jQuery("#DeviceIDRegion").text("Device ID: " + DeviceID);
 
     if (DeviceID=="ef8048728a9fcdbb")
     {
-        jQuery("#LaunchCloudVoteAnchorButton").attr("href", "http://www.meridiaars.com/cv1.html");
+        CloudVoteURL = "http://www.meridiaars.com/cv1.html");
     }
 
-    if (DeviceID == "1875fec3215046cd") {
-        jQuery("#LaunchCloudVoteAnchorButton").attr("href", "http://www.meridiaars.com/cv2.html");
+    if (DeviceID == "1875fec3215046cd") 
+    {
+       CloudVoteURL = "http://www.meridiaars.com/cv2.html");
     }
 
     //jQuery("#LaunchCloudVoteAnchorButton")[0].click();
-    jQuery('#LaunchCloudVoteAnchorButton')[0].click();
-    
-    //jQuery("#LaunchCloudVoteAnchorButton").show();
+    //jQuery('#LaunchCloudVoteAnchorButton')[0].click();
+
+    jQuery("#LaunchCloudVoteAnchorButton").attr("href", CloudVoteURL);
+    window.location.href = CloudVoteURL;
 
 }
